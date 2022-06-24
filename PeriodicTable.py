66736ce7e64380ee90f831b313c38ea58bbf5a92
hrtowii <68852354+hrtowii@users.noreply.github.com>
2022-06-24 15:10:08 +0800

    Update PeriodicTable.py

--- PeriodicTable.py
+++ PeriodicTable.py
@@ -1,38 +1,37 @@
-
 # Initially my approach was to create a class w/ properties and use eval() to add them to a dictionary to be printed. i didnt get it to work though so just directly add them to a dictionary
 # My second approach was to use 1 list for every element and try to print out the properties in the list but i kept facing some problem because the index was weird
 # Last approach which seems to work is to use 4 lists, each containing an element's specific property. Easier to use. Done w/ my friend's help
-class element(): #Each element has 4 lists, MakeElement, and display() functions
+class element(): #Each element has 4 lists, MakeElement(), and display() functions
     ElementNameList = []
     ElementSymList = []
-    ElementProlist = []
+    ElementProList = []
     ElementMassList = []
     def __init__(self):
         pass
     def MakeElement(self, name, symbol, proton, atomic):
         self.ElementNameList.append(name)
         self.ElementSymList.append(symbol)
-        self.ElementProlist.append(proton)
+        self.ElementProList.append(proton)
         self.ElementMassList.append(atomic)
-    def display(self, target): # made by friend
+    def display(self, target):
         try:
-            index = NewElement.ElementNameList.index(target) #If element name is in the list, stop. If not, continue on the try/except
+            index = NewElement.ElementNameList.index(target) #If element name is in the list?
         except:
             try: 
                 index = NewElement.ElementSymList.index(target) #If element symbol is in the list?
-            except:
+            except: #Point of failure: If two elements have a proton/mass number that is the same, this won't work
                 try:
-                    index = NewElement.ElementProlist.index(target) #If element proton number is in the list?
+                    index = NewElement.ElementProList.index(target) #If element proton number is in the list?
                 except:
                     try: 
                         index = NewElement.ElementMassList.index(target) #If element mass number is in the list?
                     except:
                         print("No value found")
                         return 0
-        print("Element Name: " + NewElement.ElementNameList[index])
-        print("Element Symbol: " + NewElement.ElementSymList[index])
-        print("Element Proton Number: " + NewElement.ElementProlist[index])
-        print("Element Mass Number: " + NewElement.ElementMassList[index])
+        print("Name: " + NewElement.ElementNameList[index])
+        print("Symbol: " + NewElement.ElementSymList[index])
+        print("Proton Number: " + NewElement.ElementProList[index])
+        print("Mass Number: " + NewElement.ElementMassList[index])
 
 
 
@@ -59,11 +58,15 @@ def pee():
         pee()
     elif operation == '2':
         def show():
-            target = input("Input name, symbol, proton number, or mass number: ")
-            NewElement.display(target)
+            target = input("Input name or symbol: ")
+            try:
+                NewElement.display(target)
+            except:
+                print("Element doesn't exist, try again")
+                pee()
         show()
         pee()
-    elif operation == '3': #HOW DO I DO THIS?
+    elif operation == '3':
         def RemoveElement(): # made by friend
             global NewElement
             RemovedElement = input("Which element to be removed? ")
@@ -73,8 +76,8 @@ def pee():
                 try: 
                     index = NewElement.ElementSymList.index(RemovedElement)
                 except:
-                    try:
-                        index = NewElement.ElementProlist.index(RemovedElement)
+                    try: #Same caveat as display()
+                        index = NewElement.ElementProList.index(RemovedElement)
                     except:
                         try: 
                             index = NewElement.ElementMassList.index(RemovedElement)
@@ -83,7 +86,7 @@ def pee():
                             return 0
             NewElement.ElementNameList.pop(index)
             NewElement.ElementSymList.pop(index)
-            NewElement.ElementProlist.pop(index)
+            NewElement.ElementProList.pop(index)
             NewElement.ElementMassList.pop(index)
             namelist = ', '.join(NewElement.ElementNameList) # print the list in a pretty way.
             print("Successfully removed. New list of elements: " + namelist)
@@ -101,7 +104,7 @@ def again():
     elif list_again == 'N' or list_again == 'n':
         print('OK. Bye bye. :)')
     else:
-        print("read properly pls")
+        print("Please read ):")
         again()
 
 pee()
